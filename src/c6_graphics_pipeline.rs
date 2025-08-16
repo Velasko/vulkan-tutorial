4dce90f8b6a329bfbc39c7745531d8e9370f6bc9
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2025-08-16 18:01:09 -0300

    Vulkano: Starting chap 6

---
+++ src/c6_graphics_pipeline.rs
@@ -0,0 +1,99 @@
+use crate::prelude::*;
+
+use vulkano::buffer::BufferContents;
+use vulkano::pipeline::graphics::vertex_input::Vertex;
+
+mod cs {
+    vulkano_shaders::shader! {
+        ty: "vertex",
+        src: r"
+            #version 460
+
+            layout(location = 0) in vec2 position;
+
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0);
+            }
+        ",
+    }
+}
+
+#[derive(BufferContents, Vertex)]
+#[repr(C)]
+struct MyVertex {
+    #[format(R32G32_SFLOAT)]
+    position: [f32; 2],
+}
+
+pub fn main() {
+    // Base initialization
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let instance = Instance::new(
+        library,
+        InstanceCreateInfo {
+            flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+            ..Default::default()
+        },
+    )
+    .expect("failed to create instance");
+
+    let physical_device = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
+        .next()
+        .expect("no devices available");
+
+    let queue_family_index = physical_device
+        .queue_family_properties()
+        .iter()
+        .enumerate()
+        .position(|(_queue_family_index, queue_family_properties)| {
+            queue_family_properties
+                .queue_flags
+                .contains(QueueFlags::GRAPHICS)
+        })
+        .expect("couldn't find a graphical queue family") as u32;
+
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            // here we pass the desired queue family to use by index
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        },
+    )
+    .expect("failed to create device");
+
+    let queue = queues.next().unwrap();
+
+    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+
+    // 6.2 Vertex input
+    let vertex1 = MyVertex {
+        position: [-0.5, -0.5],
+    };
+    let vertex2 = MyVertex {
+        position: [0.0, 0.5],
+    };
+    let vertex3 = MyVertex {
+        position: [0.5, -0.25],
+    };
+
+    let vertex_buffer = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::VERTEX_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+                | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            ..Default::default()
+        },
+        vec![vertex1, vertex2, vertex3],
+    )
+    .unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ mod c2_initialization;
 mod c3_buffers;
 mod c4_compute_pipeline;
 mod c5_using_images;
+mod c6_graphics_pipeline;
 mod prelude;
 
 fn main() {
-    c5_using_images::main();
+    c6_graphics_pipeline::main();
 }
