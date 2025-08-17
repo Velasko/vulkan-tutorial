f9f0e16b2edcffe8390f708dda6e40084e766aa1
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2025-08-17 19:59:24 -0300

    Vulkano: Cap 7 - Trianglin na janela

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ mod c3_buffers;
 mod c4_compute_pipeline;
 mod c5_using_images;
 mod c6_graphics_pipeline;
+mod c7_windowing;
 mod prelude;
 
 fn main() {
-    c6_graphics_pipeline::main();
+    c7_windowing::main();
 }
