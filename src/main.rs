79f3685efd2dab1fc47ab070e4d53936569fce0b
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2025-08-20 21:36:43 -0300

    Cleaning up

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
 #![allow(unused)]
 
-mod c2_initialization;
-mod c3_buffers;
-mod c4_compute_pipeline;
-mod c5_using_images;
-mod c6_graphics_pipeline;
-mod c7_windowing;
-mod prelude;
-
-fn main() {
-    c7_windowing::main();
-}
+fn main() {}
