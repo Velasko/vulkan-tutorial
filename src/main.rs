448465331d4743545c1efba33c6791e358d0954b
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2025-10-08 22:34:09 -0300

    Vulkanalia: 14 Shader follow through

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,12 @@ use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::window::{Window, WindowBuilder};
 
+// use vulkanalia::vk;
 use vulkanalia::vk::ExtDebugUtilsExtension;
 use vulkanalia::vk::KhrSurfaceExtension;
 use vulkanalia::vk::KhrSwapchainExtension;
+
+use vulkanalia::bytecode::Bytecode;
 
 /// Whether the validation layers should be enabled.
 const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
@@ -124,7 +127,29 @@ impl App {
     }
 }
 
+unsafe fn create_shader_module(
+    device: &Device,
+    bytecode: &[u8],
+) -> Result<vk::ShaderModule> {
+    let bytecode = Bytecode::new(bytecode).unwrap();
+    let info = vk::ShaderModuleCreateInfo::builder().code(bytecode.code()).code_size(bytecode.code_size());
+
+    Ok(device.create_shader_module(&info, None)?)
+}
+
 unsafe fn create_pipeline(device: &Device, data: &mut AppData) -> Result<()> {
+    let vert = include_bytes!("../shaders/vert.spv");
+    let frag = include_bytes!("../shaders/frag.spv");
+
+    let vert_shader_module = create_shader_module(device, &vert[..])?;
+    let frag_shader_module = create_shader_module(device, &frag[..])?;
+
+    let vert_stage = vk::PipelineShaderStageCreateInfo::builder().stage(vk::ShaderStageFlags::VERTEX).module(vert_shader_module).name(b"main\0");
+    let frag_stage = vk::PipelineShaderStageCreateInfo::builder().stage(vk::ShaderStageFlags::FRAGMENT).module(frag_shader_module).name(b"main\0");
+
+    device.destroy_shader_module(vert_shader_module, None);
+    device.destroy_shader_module(frag_shader_module, None);
+
     Ok(())
 }
 
